45abfd87aee72bdaf275b3f9efaa4e65578b5ad0
Ubuntu <ubuntu@ip-10-0-3-55.ap-southeast-1.compute.internal>
2022-07-15 09:42:48 +0000

    pipeline sorta working for samples as well

--- workflow/scripts/bamfilter.py
+++ workflow/scripts/bamfilter.py
@@ -11,12 +11,28 @@ outputfile = snakemake.output[0]
 samfile = pysam.AlignmentFile(inputfile, "rb")
 outfile = pysam.AlignmentFile(outputfile, "wb", template=samfile)
 
-for read in samfile.fetch():
-	if read.infer_query_length() != None and read.is_proper_pair:
-		len_ops, num_ops = read.get_cigar_stats()
-		if sum(len_ops[0:2])/read.infer_read_length() > 0.7 and len_ops[10]/sum(len_ops[0:3]) < 0.05:
-			outfile.write(read)
-								
-outfile.close()
-samfile.close()
+def old_filt():
+	for read in samfile.fetch():
+		if read.infer_query_length() != None and read.is_proper_pair:
+			len_ops, num_ops = read.get_cigar_stats()
+			if sum(len_ops[0:2])/read.infer_read_length() > 0.7 and len_ops[10]/sum(len_ops[0:3]) < 0.05:
+				outfile.write(read)			
+	outfile.close()
+	samfile.close()
 
+def loose_filt():
+	for read in samfile.fetch():
+		if read.infer_query_length() != None and read.is_proper_pair:
+			len_ops, num_ops = read.get_cigar_stats()
+			if sum(len_ops[0:2])/read.infer_read_length() > 0.5 and len_ops[10]/sum(len_ops[0:3]) < 0.05:
+				outfile.write(read)			
+	outfile.close()
+	samfile.close()
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		old_filt()
+	else:
+		print("lose filter")
+		loose_filt()
+		
